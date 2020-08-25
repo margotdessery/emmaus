b905b244a193685da4c34666fe041e7fbf1d71d9
Dessery Margot <margot.dessery@gmail.com>
2020-08-25 16:29:28 +0200

    authentification

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -98,7 +98,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('homepage'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl()
